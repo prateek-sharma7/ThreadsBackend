9160abd7ad84a803f04fb724a4b17250fe5377c6
Prateek <sharmaprateek499@gmail.com>
2024-07-12 23:46:20 +0530

    Setting up the project and first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { connect } from "./config/database.js";
+import Tweet from "./models/tweet.js";
+
+const app = express();
+
+app.listen(3000, async () => {
+  console.log("Server started");
+
+  connect();
+
+  console.log("Database connected.");
+
+  Tweet.create({
+    content: "This is my first tweet",
+    likes: 25,
+    noOfRetweets: 5,
+    comment: "My comment",
+  });
+});
